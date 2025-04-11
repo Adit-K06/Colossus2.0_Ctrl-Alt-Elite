2f40cd46941a3e0aa778e1a3c0ff2b78b9bedb8d
Adit-K06 <kadagadakai.adit06@gmail.com>
2025-04-12 00:24:38 +0530

    Updated the AI resume and job description analyzer agent

--- AI_Resume_Analyzer/01CVAgent.py
+++ AI_Resume_Analyzer/01CVAgent.py
@@ -3,6 +3,7 @@ load_dotenv()
 
 from agno.agent import Agent
 from agno.models.groq import Groq
+from agno.models.google import Gemini
 from knowledgebase import pdf_knowledge_base
 
 from agno.tools.duckduckgo import DuckDuckGoTools
@@ -11,30 +12,28 @@ from agno.storage.postgres import PostgresStorage
 cvReaderAgent = Agent(
     name="CVReaderAgent",
     description="You are seasoned technology interview panel  member for hiring fresh college students!",
-    model = Groq(
-        id="llama-3.3-70b-versatile",
+    model = Gemini(
+        id="gemini-2.0-flash-exp",
     ),
     markdown=True,
     knowledge=pdf_knowledge_base,
     search_knowledge=True,
     instructions=[
-        "Always only search your knowledge base for resumes. Do not provide generic answers",
+        "Always only search your knowledge base for resume and job description. Do not provide generic answers",
+        "Job description is in jd1.pfd and resume is in Resume.pdf",
         "Highlight the name of the candidate and skills and experience in the response.",
         "For the questions to be asked in interview, you can search web to get questions and answers"
     ],
     tools=[DuckDuckGoTools()],
-
+    read_chat_history=True,
+    add_history_to_messages=True
 )
 
-
-print("Loading knowledge base...")
+print("Loading Resume knowledge base...")
 cvReaderAgent.knowledge.load(recreate=False)
-print("Knowledge base loaded.")
+print("Resume Knowledge base loaded.")
 
-query = "start"
+print("This is your feedback based on your resume and job description: ")
 
-while query.lower() != "exit":
-    query = input("Enter your query: ")
-    cvReaderAgent.print_response(query)
-
-print('Bye....')
+query = "check your knowledgebase to find the answer. Check if candidate match the job description in terms of skills and experience and provide the percentage of suitability of the candidate"
+cvReaderAgent.print_response(query)
--- AI_Resume_Analyzer/knowledgebase.py
+++ AI_Resume_Analyzer/knowledgebase.py
@@ -3,7 +3,7 @@ from agno.vectordb.pgvector import PgVector
 from agno.embedder.google import GeminiEmbedder
 
 pdf_knowledge_base = PDFKnowledgeBase(
-    path="cvs/Resume.pdf",
+    path="cvs",
     vector_db=PgVector(
         table_name="cvs",
         embedder=GeminiEmbedder(dimensions=1536),
@@ -11,3 +11,13 @@ pdf_knowledge_base = PDFKnowledgeBase(
     ),
    reader=PDFReader()
 )
+
+# jd_knowledge_base = PDFKnowledgeBase(
+#     path="cvs/jd1.pdf",
+#     vector_db=PgVector(
+#         table_name="jd",
+#         embedder=GeminiEmbedder(dimensions=1536),
+#         db_url="postgresql://neondb_owner:npg_ewUNJjB7Qn8T@ep-muddy-mouse-a5a04l1u-pooler.us-east-2.aws.neon.tech/hackothon?sslmode=require",
+#     ),
+#    reader=PDFReader()
+# )
